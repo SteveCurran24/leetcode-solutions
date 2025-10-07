b765d246801653778dba3ac3c5dd0f8d9e1ec045
Steven Curran <s.curran244@gmail.com>
2025-10-07 14:38:39 -0400

    Create best-time-to-buy-and-sell-stock.py

---
+++ grind75/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        min_price = 10**4
+        max_profit=0
+
+        for i in prices:
+            if i < min_price:
+                min_price = i
+            
+            if (i - min_price) > max_profit:
+                max_profit = i - min_price
+
+        return max_profit
